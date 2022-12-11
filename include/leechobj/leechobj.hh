71568c96cc104583c44c518c6cf0ffeca8eb460c
derzhavin3016 <derzhavin.andrej@yandex.ru>
2022-12-11 21:26:19 +0300

    [leechobj] Initial

---
+++ include/leechobj/leechobj.hh
@@ -0,0 +1,7 @@
+#ifndef __INCLUDE_LEECHOBJ_LEECHOBJ_HH__
+#define __INCLUDE_LEECHOBJ_LEECHOBJ_HH__
+
+#include "common/common.hh"
+#include "common/opcodes.hh"
+
+#endif /* __INCLUDE_LEECHOBJ_LEECHOBJ_HH__ */
