431d3280b32618c76f406d5938a016a11ca9f0e7
Tako-San <dxe13@mail.ru>
2022-12-12 19:56:45 +0300

    Test

---
+++ test/unit/leechfile/leechfile_test.cc
@@ -0,0 +1,13 @@
+#include <sstream>
+#include <string_view>
+
+#include "leechfile/leechfile.hh"
+#include "test_header.hh"
+
+using namespace leech;
+
+TEST(Serialize, Empty) {
+  
+}
+
+#include "test_footer.hh"
