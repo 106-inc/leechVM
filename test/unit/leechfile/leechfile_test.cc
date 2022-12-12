d3ae59af227df371618e2436ace0ab5072aaf551
Tako-San <dxe13@mail.ru>
2022-12-12 20:09:02 +0300

    Added test

--- test/unit/leechfile/leechfile_test.cc
+++ test/unit/leechfile/leechfile_test.cc
@@ -7,7 +7,18 @@
 using namespace leech;
 
 TEST(Serialize, Empty) {
-  
+  // Assign
+  LeechFile cf{};
+  std::ostringstream oss{};
+  std::string_view ans{"theLEECH"
+                       "\x0\x0\x0\x0\x0\x0\x0\x0",
+                       16};
+
+  // Act
+  cf.serialize(oss);
+
+  // Assert
+  EXPECT_EQ(oss.str(), ans);
 }
 
 #include "test_footer.hh"
