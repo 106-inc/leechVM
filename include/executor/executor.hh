0cbbb1cd89307c0d678fc818d4d9caeab19eb7f2
Andrey Derzhavin <derzhavin.andrej@yandex.ru>
2023-06-06 23:19:54 +0300

    Add inst callbacks (#26)
    * Add inst callbacks
    
    * Fix cast
    
    * Make --dump an option, not flag
    
    * Remove redundant using decl

--- include/executor/executor.hh
+++ include/executor/executor.hh
@@ -100,11 +100,8 @@ struct State final {
   auto &getCurFrame() { return funcStack.top(); }
 };
 
-using ExecFunc = std::function<void(const Instruction &, State &)>;
-
 class Executor final {
   State state_{};
-  static const std::unordered_map<Opcodes, ExecFunc> execMap_;
 
 public:
   explicit Executor(LeechFile *leechFile) : state_(leechFile) {}
